77d6553f9524096402c0bcfcf010c13e1a46e8e1
ecakir <cakir-enes@outlook.com>
2021-06-04 23:08:05 +0300

    ch

--- src/misc/edgcQRGenerator.tsx
+++ src/misc/edgcQRGenerator.tsx
@@ -24,7 +24,7 @@ import axios from 'axios';
 import { EUDGC } from '../generated-files/dgc-combined-schema';
 
 const api = axios.create({
-    baseURL: '',
+    baseURL: 'https://temp-hpass.duckdns.org/issuance',
     headers: {
         "Content-Type": "application/json"
     },
